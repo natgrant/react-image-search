c0c418d7f34cb0a69d0622f8aa8fb6e15ee7af6d
Nat Grant <email.natgrant@gmail.com>
2019-04-05 11:26:44 +1300

    get list of images

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,13 +4,14 @@ import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends Component {
-  onSearchSubmit(userInput) {
-    axios.get("https://api.unsplash.com/search/photos", {
+  async onSearchSubmit(userInput) {
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
       params: { query: userInput },
       headers: {
         Authorization: `Client-ID 8f47b1f99137bf893d8dcb10f27906e9ddc6f4e1f8d0382d404a0ee3370cdef1`
       }
     });
+    console.log(response.data.results);
   }
 
   render() {
