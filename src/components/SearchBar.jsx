0bf0be2804e9983906be17b070d96801a28e1232
Nat Grant <email.natgrant@gmail.com>
2019-04-04 17:36:49 +1300

    search field

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,14 +1,26 @@
 import React, { Component, Fragment } from "react";
 
 export default class SearchBar extends Component {
+  state = { userInput: "" };
+
+  handleInputChange = e => {
+    this.setState({
+      userInput: e.target.value
+    });
+  };
+
+  handleFormSubmit = e => {
+    e.preventDefault();
+  };
+
   render() {
     return (
       <Fragment>
         <div className="ui segment">
-          <form className="ui form">
+          <form onSubmit={this.handleFormSubmit} className="ui form">
             <div className="field">
               <label>Image Search</label>
-              <input type="text" />
+              <input type="text" onChange={this.handleInputChange} />
             </div>
           </form>
         </div>
