3ed27160caccf7f05f57de74330fa01b224d77c1
Nat Grant <email.natgrant@gmail.com>
2019-04-04 18:18:10 +1300

    unsplash api

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,16 @@
 import React, { Component } from "react";
+import axios from "axios";
+
 import SearchBar from "./SearchBar";
 
 class App extends Component {
   onSearchSubmit(userInput) {
-    console.log(userInput);
+    axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: userInput },
+      headers: {
+        Authorization: `Client-ID 8f47b1f99137bf893d8dcb10f27906e9ddc6f4e1f8d0382d404a0ee3370cdef1`
+      }
+    });
   }
 
   render() {
