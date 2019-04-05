770b4ef8b03866641d67bdbd21fea9fafa08ee0f
Nat Grant <email.natgrant@gmail.com>
2019-04-05 15:14:16 +1300

    add grid layout

--- src/components/ImageList.jsx
+++ src/components/ImageList.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import "./ImageList.scss";
 
 export default function ImageList(props) {
-  const images = props.images.map(image => {
-    return <img key={image.id} alt="unsplash" src={image.urls.regular} />;
+  const images = props.images.map(({ id, description, urls }) => {
+    return <img key={id} alt={description} src={urls.regular} />;
   });
-  return <div>{images}</div>;
+  return <div className="image-list">{images}</div>;
 }
